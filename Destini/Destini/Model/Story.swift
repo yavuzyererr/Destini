0688bb85f5a971c74e35ce7afef6a894b224ef87
yavuz yerer <yavuz.yererr@gmail.com>
2023-01-17 13:57:49 +0300

    start project

---
+++ Destini/Destini/Model/Story.swift
@@ -0,0 +1,8 @@
+//
+//  Story.swift
+//  Destini
+//
+//  Created by Yavuz Yerer on 17.01.2023.
+//
+
+import Foundation
---
+++ Destini/Destini/Model/Storybrain.swift
@@ -0,0 +1,8 @@
+//
+//  Storybrain.swift
+//  Destini
+//
+//  Created by Yavuz Yerer on 17.01.2023.
+//
+
+import Foundation
