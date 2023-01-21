a7b3f065583e1d63e9658abdec02398c6026c6c4
yavuz yerer <yavuz.yererr@gmail.com>
2023-01-21 11:16:33 +0300

    Complete Project

--- Destini/Destini/Model/Story.swift
+++ Destini/Destini/Model/Story.swift
@@ -6,3 +6,17 @@
 //
 
 import Foundation
+struct Story {
+  let  title : String
+  let  choice1 : String
+  let  choice1Destinition : Int
+  let  choice2 : String
+  let  choice2Destinition : Int
+    init(title: String, choice1: String, choice1Destinition: Int , choice2: String , choice2Destinition : Int ) {
+        self.title = title
+        self.choice1 = choice1
+        self.choice2 = choice2
+        self.choice1Destinition = choice1Destinition
+        self.choice2Destinition = choice2Destinition
+    }
+}
--- Destini/Destini/ViewController.swift
+++ Destini/Destini/ViewController.swift
@@ -8,12 +8,30 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var storyLabel: UILabel!
+    @IBOutlet weak var choice1Button: UIButton!
+    @IBOutlet weak var choice2Button: UIButton!
+    
+    
+    var storyBrain = StoryBrain()
+    
+    
+   
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        UpdateUI()
+            }
+    
+    @IBAction func choiceMade(_ sender: UIButton){
+        storyBrain.nextStory(userChoice: sender.currentTitle!)
+        UpdateUI()
     }
-
-
+    func UpdateUI(){
+        storyLabel.text = storyBrain.getStoryText()
+        choice1Button.setTitle(storyBrain.getChoice1(), for: .normal)
+        choice2Button.setTitle(storyBrain.getChoice2(), for: .normal)
+    
+    }
 }
 
